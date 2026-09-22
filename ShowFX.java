84afe42c3ae3be6f34ff26ee5f03707a9ce1f7eb
boehmcon <constantin.boehm@hsf-meissen.de>
2026-09-22 12:56:14 +0200

    Update 2.0
    great change

--- ShowFX.java
+++ ShowFX.java
@@ -166,5 +166,5 @@ In AWT konnte Graphics direkt auf den Hintergrund bzw. die Komponente zeichnen.
 Beim JavaFX-Canvas muss der vorherige Inhalt explizit gelöscht werden. Deshalb:
 g.setFill(Color.BLACK);
 g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight()); 
-ich kann nicht mehr
+lol
 */
